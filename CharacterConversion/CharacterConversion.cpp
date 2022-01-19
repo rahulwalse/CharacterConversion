1427881d93a1e1adfd73b1de96264acc9b9d076e
Rahul Walse <rahul_walse@hotmail.com>
2022-01-19 16:33:12 +0530

    Provided option in tester to convert given decimal number to its equivalent character string representation.

--- CharacterConversion/CharacterConversion.cpp
+++ CharacterConversion/CharacterConversion.cpp
@@ -17,12 +17,14 @@ int main()
         std::cout << "-";
 
     int choice; bool exitStatus = false;    std::string charString;
+    double decimalNumber = 0;
     while(!exitStatus)
     {
         /* Display the menu to the user. */
         std::cout << "\n1. Convert string of characters to its equivalent decimal representation";
         std::cout << "\n2. Convert string of characters to its equivalent decimal representation including decimal point";
-        std::cout << "\n3. Exit\nEnter your choice: ";
+        std::cout << "\n3. Convert decimal number to its equivalent characters (string) representation";
+        std::cout << "\n4. Exit\nEnter your choice: ";
         std::cin >> choice;
 
         switch (choice)
@@ -37,7 +39,12 @@ int main()
                     std::cout << "\nDecimal representation: " << CharToDecimal(charString) << "\n";
                     break;
 
-            case 3: std::cout << "\nExiting the program...\n";
+            case 3: std::cout << "\nNote: current precision is 5 digits after decimal point.\nDecimal number: ";
+                    std::cin >> decimalNumber;
+                    std::cout << "\nCharacter string representation: " << DecimalToChar(decimalNumber);
+                    break;
+
+            case 4: std::cout << "\nExiting the program...\n";
                     exitStatus = true;
                     break;
 
