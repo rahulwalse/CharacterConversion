d6f67eb43d1e49c3e864e43eeedf2c50ed5d0a0b
Rahul Walse <rahul_walse@hotmail.com>
2022-01-19 16:32:25 +0530

    Declarations of additional functions for reversing digits, converting decimal part to whole number and string creator.

--- CharacterConversion/AuxFunc.h
+++ CharacterConversion/AuxFunc.h
@@ -8,12 +8,26 @@
 
 #include <string>
 
+/* Function to get the position of decimal point in given string. */
 int DecimalPosition(const std::string&);
 
+/* Functions to construct strings before and after decimal point in given string. */
 std::string GetIntegerString(const std::string&, const std::string::size_type&);
 std::string GetDecimalString(const std::string&, const std::string::size_type&);
 
+/* Function to convert given number to its representation as decimal part. */
 double ConvertToDecimal(const long long&);
+
+/* Function to get the number digits in given number. */
 int DigitCount(const long long&);
+
+/* Function to convert given decimal part of number to whole number. */
+void DecimalToInteger(double&);
+
+/* Function to reverse digits of given number. */
+long ReverseDigits(const long&);
+
+/* Function to create string from the given number. */
+std::string StringCreator(const long&);
 
 #endif // !AUXFUNC_H
